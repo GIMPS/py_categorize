36927380ea8b4f020952f68d828ae15f636e6185
Jason Ren <renjiawei1997@gmail.com>
2018-03-13 11:32:26 +0800

    removed unnecessary import

--- test.py
+++ test.py
@@ -2,16 +2,9 @@ from __future__ import print_function, division
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
 from torch.autograd import Variable
-import numpy as np
-import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
 import os
-import copy
 import operator
 from class_name import class_names
 from mapping import name_mapping
